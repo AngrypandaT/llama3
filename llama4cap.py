149d3940c4cd25d16a8d2b58362710637ebeded6
zht <841969893@qq.com>
2024-06-30 23:15:56 +0800

    按相同格式输出到txt，并且增加进度显示

--- llama4cap.py
+++ llama4cap.py
@@ -1,5 +1,6 @@
 import ollama
 import json
+from tqdm import tqdm
 
 text_path = '/root/autodl-tmp/all_captions.txt'
 text_loader = {}
@@ -9,25 +10,11 @@ with open(text_path, 'r') as f:
             line_data = json.loads(line.strip())
             text_loader.update(line_data)
 text_loader = dict(sorted(text_loader.items()))
-video_name = {}
-answer_list = []
-
-def stream_response(generator):
-    buffer = ""
-    for chunk in generator:
-        if 'message' in chunk:
-            buffer += chunk['message']['content']
-            words = buffer.split(" ")
-            # 保留最后一个单词在缓冲区中，因为它可能是不完整的
-            buffer = words.pop() if words[-1] != '' else ""
-            for word in words:
-                if word:  # 过滤掉空字符串
-                    yield word + " "
-    # 输出最后剩余的完整单词
-    if buffer:
-        yield buffer
+result_dict = {}
+progress_bar = tqdm(total=len(text_loader), desc='Processing items')
 
 for item in text_loader:
+    answer_list = []
     for sentence in text_loader[item]:
         sentence = sentence
         labels = 'Normal,Abuse,Arrest,Arson,Assault, Burglary, Explosion, Fighting, RoadAccidents, Robbery, Shooting, Shoplifting, Stealing, Vandalism'
@@ -38,6 +25,22 @@ for item in text_loader:
             messages=messages,
             stream=True,  # 开启流式响应
         )
-        for word in stream_response(stream):
-            answer_list.append(word.strip())
-            print(answer_list)
+
+        buffer = ""
+        for chunk in stream:
+            if 'message' in chunk:
+                buffer += chunk['message']['content']
+                words = buffer.split(" ")
+                # 保留最后一个单词在缓冲区中，因为它可能是不完整的
+                buffer = words.pop() if words[-1] != '' else ""
+                for word in words:
+                    if word:  # 过滤掉空字符串
+                        answer_list.append(word.strip())
+        # 输出最后剩余的完整单词
+        if buffer:
+            answer_list.append(buffer.strip())
+    result_dict[item] = answer_list
+    progress_bar.update(1)
+progress_bar.close()
+with open('result.txt', 'w') as f:
+        f.write(json.dumps(result_dict, ensure_ascii=False, indent=4))
